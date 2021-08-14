fefd45b22370b9f9f1056feee9ee196ecbcbc720
Jihyung Kim <kimjihyung3@gmail.com>
2021-08-14 14:33:30 +1000

    Updated payments serializers.

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,16 +1,31 @@
 from rest_framework import serializers
-from djstripe.models import Subscription, Price
+from djstripe.models import Subscription, Plan, Product
 
 
-# class PriceSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['name']
 
+
+class PlanSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = Plan
+        fields = [
+            'amount_decimal', 'currency', 'interval',
+            'interval_count', 'product'
+        ]
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
+
     class Meta:
         model = Subscription
         fields = [
-            'id', 'start_date', 'cancel_At', 'cancel_at_period_end',
-            'cancelled_at', 'current_period_start',
-            'current_period_end', 'ended_at', 'status'#, 'price'
+            'id', 'created', 'cancel_at', 'cancel_at_period_end',
+            'canceled_at', 'current_period_start',
+            'current_period_end', 'ended_at', 'status', 'plan'
         ]
